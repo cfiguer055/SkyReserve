cb93a56c5de7008be91561addba7799d69495dac
cfiguer055 <cfiguer055@gmail.com>
2024-10-19 23:40:14 -0700

    Start - Adding Spanish and Italian comments

--- app/src/main/java/com/example/skyreserve/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/skyreserve/ui/home/HomeActivity.kt
@@ -32,11 +32,32 @@ import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 import javax.inject.Inject
 
+
+/**
+ * ES:
+ * Actividad principal para la pantalla de inicio de la aplicación. Maneja interacciones
+ * de usuario, búsqueda de vuelos y configuración de perfil. Esta actividad sirve como punto de
+ * entrada para la experiencia del usuario.
+ *
+ * @property
+ *
+ * -----------------------
+ *
+ * IT:
+ * Attivitá principale per la schermata iniziale dell'applicazione. Gestisce le
+ * interazioni dell'utente, la rivera di voli e la configurazione del profilo. Questa activitá
+ * funge da punto di ingresso per l'esperienza utente.
+ */
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
-    // This is the request code you will use when launching the FlightSearchActivity
-    private val FLIGHT_SEARCH_REQUEST_CODE = 1  // This can be any integer unique to the Activity
+    // ES: Este es el código de solicitud que utilizarás al iniciar el FlightSearchActivity
+    // IT: Questo é il codice di richiesta che utilizzarai quando lanci FlightSearchAcvitivy
+
+    // ES: Esto puede ser cualquier entero único para a la actividad
+    // IT: Questo puó essere un intero qualsiasi unico per l'Attivitá
+    private val FLIGHT_SEARCH_REQUEST_CODE = 1
+
     private lateinit var binding: ActivityHomeBinding
 
     @Inject
@@ -56,20 +77,52 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var email: String
 
-    // Initialize the ActivityResultLauncher
+    /**
+     * Registers a result handler for [FlightSearchActivity] that processes the user's flight search
+     * selections and updates the UI accordingly.
+     *
+     * ES:
+     * Registra un manejador de resultado para [FlightSearchActivity] que procesa las selecciones de
+     * búsqueda de vuelo del usuario y actualiza la interfaz de usuario en consecencia.
+     *
+     * IT:
+     * Registra un gestore di risultati per [FlightSearchActivity] che elabora le selezioni di ricerca
+     * del volo dell'utenete e aggiorna di conseguenza l'interfaccia utente.
+     *
+     * */
     private val startFlightSearchActivity = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            // Handle the data from FlightSearchActivity
+            // ES: Manejar los datos de FlightSearchActivity
+            // IT: Gestire i dati da FlightSearchActivity
+
             val data = result.data
-            // Use data to get the extras and do something with them
+
+            // ES: Usa los datos para obtener los extras y hacer algo con ellos
+            // IT: Usa i dati per ottenere gli extra e fare qualcosa con essi
             val tripType = data?.getStringExtra("ROUND_TRIP")
             val departAirport = data?.getStringExtra("DEPART_AIRPORT")
-            // ... and so on with other extras
+
+            // ... ES: y así con otros extras
+            // ... IT: e cosí via con altri extra
         }
     }
 
+
+    /**
+     * Called when the activity is first created. Sets up the layout, initializes session data,
+     * and handles user interaction for the home screen.
+     *
+     * ES:
+     * Se llama cuando se crea la actividad por primera vez. Configura el disño. Inicializa los
+     * datos de sesión, y gestiona la interacción del usuario para la pantalla principal.
+     *
+     * IT:
+     * Viene chiamato quando l'attivitá viene creata per la prima volta. Configura il layout,
+     * inizializza i dati di sessione e gestisce l'interazione dell'utente per la schermata principale.
+     *
+     * */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -94,10 +147,14 @@ class HomeActivity : AppCompatActivity() {
             fetchUserDetailsAfterSignIn()
         }
 
-        // Validate session and get user email
+        // EN: Validate session and get user email
+        // ES: Validar la sessión del usuario y recuperar el correo electronicó del usuario
+        // IT: Convalidare la sessione utente e recuparare l'email dell'utente
         if (authViewModel.validateSession()) {
-            // Use userEmail to fetch user details if needed
 
+            // EN: Use userEmail to fetch user details if needed
+            // ES: Usar userEmail para obtener los detalles del usuario si es necesario
+            // IT: Usare userEmail per recuperare i dettagli dell'utente, se necessario
             email = sessionManager.getUserEmail().toString()
             userAccountViewModel.fetchUserDetails(email)
 
@@ -106,31 +163,49 @@ class HomeActivity : AppCompatActivity() {
             }
 
         } else {
-            // Handle invalid session, e.g., navigate to login
+            // EN: Handle invalid session, e.g., navigate to login
+            // ES: Manejar sessión invalida limpiando la interfaz de usuario
+            // IT: Gestire sessione non valida cancellado l'interfaccia utente
             email = ""
             binding.nameText.text = ""
         }
 
         setupBottomNavigation()
 
-       // logger = (application as MyApp).logger
+        // logger = (application as MyApp).logger
         //logger.logInteraction("HomeActivity:")
 
         binding.oneWayRadioButton.setOnClickListener {
+            // EN: Handle one-way trip radio button click
+            // ES: Manejar clic en el botón de opción de viaje de ida
+            // IT: Gestire il clic sul pulsante di opzione per viaggio di sola andata
+
             //logger.logInteraction("Button clicked: ${binding.oneWayRadioButton.text}")
         }
 
         binding.departButton.setOnClickListener {
+            // EN: Handle departure button click
+            // ES: Manejar clic en el butón de salida
+            // IT: Gestire il clic sul pulsante di partenza
+
             //logger.logInteraction("Button clicked: ${binding.departButton.id}")
             showAirportAutoCompleteDialog(true)
         }
 
         binding.arriveButton.setOnClickListener {
+            // EN: Handle arrival button click
+            // ES: Manejar clic en el butóon de llegada
+            // IT: Gestire il clic sul pulsante di arrivo
+
             //logger.logInteraction("Button clicked: ${binding.arriveButton.id}")
             showAirportAutoCompleteDialog(false)
         }
 
         binding.departureDateEditText.setOnClickListener {
+            // EN: Show window for date picker dialog for selecting departure date
+            // ES: Mostrar cuadro de diálogo para seleccionar la fecha de salida
+            // IT: Mostrare finestra di dialogo per selezionare la data di partenza
+
             //logger.logInteraction("Button clicked: ${binding.departureDateEditText.id}")
             val calendar = Calendar.getInstance()
             val datePickerDialog = DatePickerDialog(
@@ -148,6 +223,10 @@ class HomeActivity : AppCompatActivity() {
         }
 
         binding.returnDateEditText.setOnClickListener {
+            // EN: Show date picker dialog for selecting return date
+            // ES: Mostrar cuadro de diálogo para seleccionar la fecha de regreso
+            // IT: Mostare finestra di dialogo per selezionare la data di ritorno
+
             val calendar = Calendar.getInstance()
             val datePickerDialog = DatePickerDialog(
                 this,
@@ -166,6 +245,10 @@ class HomeActivity : AppCompatActivity() {
         binding.passengerCountTextView.text = passengerCount.toString()
 
         binding.decrementPassengerButton.setOnClickListener {
+            // EN: Decrease passenger count
+            // ES: Disminuir el número de pasajeros
+            // IT: Diminuire il numero di passeggeri
+
             //logger.logInteraction("Button clicked: Decrement(${binding.decrementPassengerButton.text})")
             if (passengerCount > 1) {
                 passengerCount--
@@ -175,6 +258,10 @@ class HomeActivity : AppCompatActivity() {
         }
 
         binding.incrementPassengerButton.setOnClickListener {
+            // EN: Increase passenger count
+            // ES: Aumentar el número de pasajeros
+            // IT: Aumentare il numero di passeggeri
+
             //logger.logInteraction("Button clicked: Increment(${binding.incrementPassengerButton.text})")
             if (passengerCount < maxPassengerCount) {
                 passengerCount++
@@ -184,12 +271,28 @@ class HomeActivity : AppCompatActivity() {
         }
 
         binding.searchFlightButton.setOnClickListener {
+            // EN: Initiate flight search
+            // ES: Iniciar búsqueda de vuelos
+            // IT: Avviare la ricerca di voli
+
             //logger.logInteraction("Button clicked: ${binding.searchFlightButton.text}")
             navigateToFlightSearch()
         }
 
     }
 
+
+    /**
+     * EN:
+     * Navigates to the [FlightSearchActivity] with the current flight search parameters.
+     *
+     * ES:
+     * Navega a [FlightSearchActivity] con los parámetros de búsqueda de vuelo actuales.
+     *
+     * IT:
+     * Naviga a [FlightSearchActivity] con i parametri di riecera volo correnti.
+     *
+     */
     private fun navigateToFlightSearch() {
         val intent = Intent(this, FlightSearchActivity::class.java)
         // DONT UNCOMMENT intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -219,12 +322,42 @@ class HomeActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    /**
+     * EN:
+     * Navigates to the [AccountActivity] to manage user account information.
+     *
+     * ES:
+     * Navega a [AccountActivity] para gestionar la informatción de la cuenta del usuario.
+     *
+     * IT:
+     * Naviga a [AccountActivity] per gestire le informazioni dell'account utente.
+     *
+     * */
     private fun navigateToAccount() {
         val intent = Intent(this, AccountActivity::class.java)
         startActivity(intent)
     }
 
-    // Helper function to get month short name
+
+
+    /**
+     * EN:
+     * Retrieves the short name (abbreviation) of a month based on the given month index.
+     * The index should be an integer between 0 (January) and 11 (December).
+     * @param month The index of the month (0 for January, 11 for December).
+     * @return A string representing the abbreviated name of the month.
+     *
+     * Español: Recupera el nombre corto (abreviatura) de un mes basado en el índice dado.
+     * El índice debe ser un número entero entre 0 (enero) y 11 (diciembre).
+     * @param month El indice del mes (0 para enero, 11 para diciembre).
+     * @return Una cadena que representa el nombre abreviado del mes.
+     *
+     * Italiano: Recupera il nome abbreviato di un mese in base all'indice fornito.
+     * L'indice deve essere un numero intero tra 0 (gennaio) e 11 (dicembre).
+     * @param month L'indice del mese (0 per gennaio, 11 per dicembre).
+     * @return Una stringa che rappresenta il nome abbreviato del mese.
+     *
+     */
     fun getMonthShortName(month: Int): String {
         val dateFormat = SimpleDateFormat("MMM", Locale.getDefault())
         val calendar = Calendar.getInstance()
@@ -232,6 +365,18 @@ class HomeActivity : AppCompatActivity() {
         return dateFormat.format(calendar.time)
     }
 
+    /**
+     * EN:
+     * Displays a dialog for the user to complete their profile information after signing up.
+     *
+     * ES:
+     * Muestra cuadro de diálogo para que el usuario complete su información después de registrarse.
+     *
+     * IT:
+     * Mostra una finestra di dialogo per consentire all'utente di completare le informazioni del
+     * profilo dopo la registrazione.
+     *
+     * */
     private fun showSignUpDialog() {
         val binding = DialogSignUpBinding.inflate(layoutInflater)
         val dialog = AlertDialog.Builder(this).apply {
@@ -239,6 +384,7 @@ class HomeActivity : AppCompatActivity() {
             setView(binding.root)
             setPositiveButton("Submit") { _, _ ->
                 // Get the user's email from session manager
+
                 //val userEmail = LocalSessionManager(this@HomeActivity).getUserEmail() ?: return@setPositiveButton
 
                 email = userAccountViewModel.getUserEmail().toString()
