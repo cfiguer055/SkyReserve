028d06d13226f807be0913558817a971a2e35a6c
cfiguer055 <cfiguer055@gmail.com>
2024-10-24 20:41:06 -0700

    commenti per il frammento di pianificazione di un viaggio - PlanATripFragment

--- app/src/main/java/com/example/skyreserve/ui/welcome/PlanATripFragment.kt
+++ app/src/main/java/com/example/skyreserve/ui/welcome/PlanATripFragment.kt
@@ -8,14 +8,11 @@ import androidx.fragment.app.Fragment
 import com.example.skyreserve.R
 
 /**
- * EN:
- * Fragment that guides the user on how to plan a trip. Part of the welcome screen sequence.
+ * EN: Fragment that guides the user on how to plan a trip. Part of the welcome screen sequence.
  *
- * ES:
+ * ES: Fragmento que guía al usuario sobre cómo planificar un viaje. Parte de la secuencia de bienvenida.
  *
- *
- * IT:
- *
+ * IT: Frammenti che guida l'utente su come pianificare un viaggio. Parte della sequenza di benvenudo.
  *
  * */
 class PlanATripFragment : Fragment() {
@@ -25,8 +22,8 @@ class PlanATripFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // EN: Inflate the layout for this fragment
-        // ES:
-        // IT:
+        // ES: Inflar el diseño para este fragmento
+        // IT: Espandere il layout per questo frammento
         return inflater.inflate(R.layout.fragment_plan_a_trip, container, false)
     }
 }
